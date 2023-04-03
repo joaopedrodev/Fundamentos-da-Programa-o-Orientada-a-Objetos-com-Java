5e8b82a9986619c1d37d5967cb48ad5fe4d6d5e2
Joaopedrodev <joaopedrodacostavale@gmail.com>
2023-04-03 13:59:35 -0300

    Aula de Java sobre construtores pela Dio

---
+++ construtores/construtores/src/SistemaCadastro.java
@@ -0,0 +1,18 @@
+
+
+public class SistemaCadastro {
+	public static void main(String[] args) {
+		//criamos uma pessoa no sistema
+		Pessoa marcos = new Pessoa("123", "Marcos");
+		
+		//definimos o endereco de marcos
+		marcos.setEndereco("RUA DAS MARIAS");
+				
+	    //e como definir o nome e cpf do marcos ?
+				
+	   //imprimindo o marcos sem nome e cpf 
+				
+	 System.out.println(marcos.getNome() + " - " + marcos.getCpf()  + "  Seu endereço é " + marcos.getEndereco());		
+	}
+}
+/* Construtores */
